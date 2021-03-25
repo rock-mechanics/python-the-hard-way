f3b65727e79a5de4b10c7e7dba3693052efb39ef
Jing Chen <jingchen@tutanota.com>
2021-03-25 09:53:01 +0800

    Ex1-5

---
+++ ex3.py
@@ -0,0 +1,20 @@
+print("I will now count my chickens : ")
+
+print("Hens {}".format(25 + 30 // 6))
+print("Roosters {}".format(100 - 25 * 3 % 4))
+
+print("Now I will count the eggs : ")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 // 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7 ?")
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2 ? {}".format( 3 + 2 ))
+
+print("What is 5 - 7 ? {}".format( 5 - 7 ))
+print("Oh, that is why it is false")
+print("How about some more.")
+
+print("Is it greater? {}".format( 5 > -2))
+print("Is it greater or equal? {}".format( 5 >= -2))
+print("Is it less or equal? {}".format(5 <= -2))
